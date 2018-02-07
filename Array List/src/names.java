c7b564fc9883992946e0661433b0bd7c3cf2bdb3
abernardy151 <35156786+abernardy151@users.noreply.github.com>
2018-02-06 19:11:38 -0500

    NameArray

---
+++ Array List/src/names.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class names {
+	public static void main (String[] args){
+		ArrayList<String> teachers = new ArrayList<String>();
+		
+	teachers.add("Anthony");
+	teachers.add("Paul");
+	teachers.add("John");
+	teachers.add("Martin");
+	
+	
+	System.out.println( teachers.get(0));
+	System.out.println( teachers.get(1));
+	System.out.println( teachers.get(2));
+	System.out.println( teachers.get(3));
+	
+	
+	}
+}
